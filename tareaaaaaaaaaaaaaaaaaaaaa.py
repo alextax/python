2565e7be73979ba1ace5d57d59941f40460ed90c
alextax <omartax.69@gmail.com>
2021-03-16 07:29:37 -0600

    xd

--- tareaaaaaaaaaaaaaaaaaaaaa.py
+++ tareaaaaaaaaaaaaaaaaaaaaa.py
@@ -1,14 +1,16 @@
 # -*- coding: utf-8 -*-
 """
-Created on Tue Mar  9 20:43:26 2021
+Created on Tue Mar  9 22:06:03 2021
 
-@author: aaaa-PC
+@author: alex-PC
 """
 
-import matplotlib.pyplot as plt
 import numpy as np
-def obtenerDiferencial(p,w,t):
-    a = np.dot(p.T, w)  # lo que obtenemos de la neurona
+import matplotlib.pyplot as plt
+from sympy import plot_implicit, symbols, Eq, And
+x, y = symbols('x y')
+def diferencial(p,w,t):
+    a = np.dot(p.T, w) 
     if a >= 0:
         a = 1
     else:
@@ -29,34 +31,46 @@ print("Patron 1")
 w= np.array([0.5,0.5,0.5]) #peso anterior
           # x1, x2, t
 p1 = np.array([3,1,0]) #patron 1
-print("Patron: ",p1," Pesos: ",w)
+print(" W: ",dw)
 t=0
 e=0
 
-dw,e = obtenerDiferencial(p1,w,t)
+dw,e = diferencial(p1,w,t)
 print("Diferencial: ",dw)#Diferencial de pesos gradiante
 print("Erorr: ",e,"\n")#Error
 
 print("Patron 2")
 p2 = np.array([0,-1,1])
-print("Patron: ",p2," Pesos: ",dw)
+print(" W: ",dw)
 t =1
-dw,e = obtenerDiferencial(p2,dw,t)
+dw,e = diferencial(p2,dw,t)
 print("Diferencial: ",dw)
 print("Error: ",e,"\n")#Error
 
 print("Patron 3")
 p3 = np.array([-2,1,0])
-print("Patron: ",p3," Pesos: ",dw)
+print(" W: ",dw)
 t=0
-dw,e = obtenerDiferencial(p2,dw,t)
+dw,e = diferencial(p2,dw,t)
 print("Diferencial: ",dw)
 print("Error: ",e,"\n")#Error
 
 print("Patron 4")
 p4 = np.array([0,2,0])
-print("Patron: ",p4," Pesos: ",dw)
+print(" W: ",dw)
 t=0
-dw ,e= obtenerDiferencial(p2,w,t)
+dw ,e= diferencial(p2,w,t)
 print("Diferencial: ",dw)
 print("Error: ",e)#Error
+
+p=plot_implicit(dw[0]+ dw[1]*x+dw[2]*y >=0)
+
+
+
+
+
+
+
+
+
+
